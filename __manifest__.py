52dd4a350c6dacd14bc11364f1c313513cbcf200
Dali <psykodali@gmail.com>
2025-12-22 23:14:05 +0100

    taxes and payment terms mapping

--- __manifest__.py
+++ __manifest__.py
@@ -17,6 +17,8 @@
         'security/ir.model.access.csv',
         'views/res_config_settings_views.xml',
         'views/account_move_views.xml',
+        'views/account_tax_views.xml',
+        'views/account_payment_term_views.xml',
     ],
     'installable': True,
     'application': False,
