22402a807a9bdfa432fd607ca80c95bd3e561abc
Dali <psykodali@gmail.com>
2026-01-13 13:21:23 +0100

    Release Candidate 1

--- models/ngsign_developer_settings.py
+++ models/ngsign_developer_settings.py
@@ -9,6 +9,7 @@ class NGSignDeveloperSettings(models.TransientModel):
     show_invoice_uuid = fields.Boolean(string='Show NGSign Invoice UUID')
     generate_pdf_debug = fields.Boolean(string='Generate PDF when debugging', help='Include base64 PDF in debug JSON')
     show_report_debug_info = fields.Boolean(string='Show Report Debug Info', help='Show yellow debug box with variable values on invoice report')
+    use_v2_endpoint = fields.Boolean(string='Use V2 Seal Endpoint', help='Use V2 endpoint (no PDF upload, local stamping)')
 
     @api.model
     def default_get(self, fields_list):
@@ -22,6 +23,7 @@ class NGSignDeveloperSettings(models.TransientModel):
             'show_invoice_uuid': params.get_param('ngsign.show_invoice_uuid', 'False') == 'True',
             'generate_pdf_debug': params.get_param('ngsign.debug_include_pdf', 'False') == 'True',
             'show_report_debug_info': params.get_param('ngsign.show_report_debug_info', 'False') == 'True',
+            'use_v2_endpoint': params.get_param('ngsign.use_v2_endpoint', 'False') == 'True',
         })
         return res
 
@@ -33,5 +35,6 @@ class NGSignDeveloperSettings(models.TransientModel):
         params.set_param('ngsign.show_invoice_uuid', str(self.show_invoice_uuid))
         params.set_param('ngsign.debug_include_pdf', str(self.generate_pdf_debug))
         params.set_param('ngsign.show_report_debug_info', str(self.show_report_debug_info))
+        params.set_param('ngsign.use_v2_endpoint', str(self.use_v2_endpoint))
         
         return {'type': 'ir.actions.act_window_close'}
