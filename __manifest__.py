6848a57296ccd7a315bcfaa2a4ac56164c1eb0ff
Dali <psykodali@gmail.com>
2026-03-05 14:26:20 +0100

    fix: (ngsign_einvoice_odoo.action_ngsign_sign_options_wizard)d reference for the new button, the wizard didn't exist in the system yet.

--- __manifest__.py
+++ __manifest__.py
@@ -16,11 +16,11 @@
     'data': [
         'security/ir.model.access.csv',
         'views/res_config_settings_views.xml',
+        'views/ngsign_sign_options_wizard_views.xml',
         'views/account_move_views.xml',
         'views/account_tax_views.xml',
         'views/account_payment_term_views.xml',
         'views/report_invoice.xml',
-        'views/ngsign_sign_options_wizard_views.xml',
         'data/mail_template_data.xml',
         # 'data/ngsign_setup.xml',
         'views/res_partner_views.xml',
